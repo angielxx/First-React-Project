302ec0d93b2c99afad5b81f235e4b80b6d50049d
angielxx <angielxx94@gmail.com>
2022-12-06 22:09:35 +0900

    section 5 : 연습문제2

--- 01-starting-setup/src/components/Expenses/ExpenseList.js
+++ 01-starting-setup/src/components/Expenses/ExpenseList.js
@@ -1,18 +1,27 @@
 import ExpenseItem from "./ExpenseItem";
-// import './ExpenseList.css'
+
 
 function ExpenseList(props) {
     const expenses = props.expenses
     const filtered_expenses = expenses.filter(expense => {
         return expense.date.getFullYear() == props.filter;
     })
-    return (filtered_expenses.map(expense => 
-        <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-        />))
+
+    let content = <p>No Expenses found</p>;
+    if (filtered_expenses.length > 0) {
+        content = filtered_expenses.map(expense => 
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />)
+    }
+    return (
+        <div>
+            {content}
+        </div>
+        )
 }
 
 export default ExpenseList;
--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -32,32 +32,44 @@ const ExpenseForm = (props) => {
         setNewTitle('');
         setNewAmount('');
         setNewDate('');
+
+        setContent(<button onClick={onButtonClickHandler}>Add New Expense</button>)
     };
 
+    const [content, setContent] = useState(<button onClick={onButtonClickHandler}>Add New Expense</button>)
+
+    function onButtonClickHandler() {
+        setContent((
+            <form onSubmit={submitHandler}>
+                <div className="new-expense__controls">
+                    <div className="new-expense__control">
+                        <label htmlFor="">Title</label>
+                        <input
+                            type="text"
+                            value={newTitle}
+                            onChange={titleChangeHandler} 
+                        />
+                    </div>
+                    <div className="new-expense__control">
+                        <label htmlFor="">Amount</label>
+                        <input type="number" min="0.01" step="0.01" value={newAmount} onChange={amountChangeHandler}/>
+                    </div>
+                    <div className="new-expense__control">
+                        <label htmlFor="">Date</label>
+                        <input type="date" min="2019-01-01" max="2022-12-31" value={newDate} onChange={dateChangeHandler}/>
+                    </div>
+                </div>
+                <div className="new-expense__actions">
+                    <button type="submit" >Add Expense</button>
+                </div>
+            </form>
+        ))
+    }
+
     return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label htmlFor="">Title</label>
-                    <input
-                        type="text"
-                        value={newTitle}
-                        onChange={titleChangeHandler} 
-                    />
-                </div>
-                <div className="new-expense__control">
-                    <label htmlFor="">Amount</label>
-                    <input type="number" min="0.01" step="0.01" value={newAmount} onChange={amountChangeHandler}/>
-                </div>
-                <div className="new-expense__control">
-                    <label htmlFor="">Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" value={newDate} onChange={dateChangeHandler}/>
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type="submit" >Add Expense</button>
-            </div>
-        </form>
+        <div>
+            {content}
+        </div>
     )
 };
 
